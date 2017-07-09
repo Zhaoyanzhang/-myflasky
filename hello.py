7f798f8d729226bb85d61f4f667f4b87cfe1fb0a
zhaoyanzhang <459257668@qq.com>
2017-07-09 21:29:20 +0800

    4a

--- hello.py
+++ hello.py
@@ -1,17 +1,41 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SECRET_KEY']='hard to guess string'
 
-@app.route('/')
+manager = Manager(app)
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/',methods=['GET','POST'])
 def index():
-	return '<h1>hello world!</h1>'
+	name =None
+	form =NameForm()
+	if form.validate_on_submit():
+		name=form.name.data
+		form.name.data=''
+	return render_template('index.html',form=form,name=name)
+
 @app.route('/1')
 def index1():
-	return '<h1>first page</h1>'
+	return render_template('1.html',current_time=datetime.utcnow())
+
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>hello, %s</h1>'% name
+	return render_template('user.html',name=name+'a')
 
 @app.route('/useragent')
 def index2():
@@ -20,5 +44,5 @@ def index2():
 
 
 if __name__=='__main__':
-	app.run(debug=True)
+	manager.run()
 
