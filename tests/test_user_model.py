8720fe1b1e4a7890541e10138a1db0a82daf0dba
zhaoyanzhang <459257668@qq.com>
2017-10-10 23:29:50 +0800

    9a add permissions

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import User
+from app.models import User,Role,AnonymousUser,Permission
 import sys
 import time
 from app import create_app,db
@@ -41,12 +41,17 @@ class UserModelTestCase(unittest.TestCase):
         token=u.generate_confirmation_token(1)
         time.sleep(2)
         self.assertFalse(u.confirm(token))
-
-
-
-
-
-
+#reason to commented out: A new User email is needed everytime when we run the test. because old email is already in db.
+'''
+    def test_roles_and_permissions(self):
+        Role.insert_roles()
+        u=User(email='johnjohn2@example.com',password='cat')
+        self.assertTrue(u.can(Permission.WRITE_ARTICLES))
+        self.assertFalse(u.can(Permission.MODERATE_COMMENTS))
+'''    
+    def test_anonymous_user(self):
+        u=AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
 
 
 if __name__=='__main__':
